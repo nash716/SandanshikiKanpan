7eef4f211c02fdab6a601adc55f5fc9554aa9774
nash <preflight.cc@gmail.com>
2014-03-13 01:24:31 +0900

    プロキシサーバ設定用拡張機能を実装

---
+++ src2/background.js
@@ -0,0 +1,17 @@
+﻿var config = {
+	mode: 'pac_script',
+	pacScript: {
+		data: 'function FindProxyForURL(url, host) {\n' +
+		      '  var regexp = /\\/kcsapi\\/api_/;' +
+		      '  if (regexp.test(url)) {\n' +
+		      '    return "PROXY localhost:8549";\n' +
+		      '  }' +
+		      '  return "DIRECT";\n' +
+		      '}'
+	}
+};
+
+chrome.proxy.settings.set({
+	value: config,
+	scope: 'regular'
+}, function() {});
