3fa8310ad4fe5886ae2cf30af9e7521d479f4323
nash <preflight.cc@gmail.com>
2014-03-18 04:35:40 +0900

    少しだけリファクタリング的な何かをした

--- src2/app/background.js
+++ src2/app/background.js
@@ -1,42 +1,55 @@
-﻿var port, port2; // port: proxy <-> background page, port2: background page <-> extension window (window.html)
-
-var config = {
-	mode: 'pac_script',
-	pacScript: {
-		data: 'function FindProxyForURL(url, host) {\n' +
-		      '  var regexp = /\\/kcsapi\\/api_/;' +
-		      '  if (regexp.test(url)) {\n' +
-		      '    return "PROXY localhost:8549";\n' +
-		      '  }' +
-		      '  return "DIRECT";\n' +
-		      '}'
-	}
+﻿var Background = {
+	port: null, // proxy <-> background page
+	port2: null // background page <-> extension window (window.html)
 };
 
-chrome.proxy.settings.set({
-	value: config,
-	scope: 'regular'
-}, function() {});
-
-chrome.runtime.onConnectExternal.addListener(function(p) {
-	port = p;
-	
-	chrome.windows.create({
-		type: 'popup',
-		url: '../browser/window.html',
-		width: 1100,
-		height: 480
-	});
-	
-	port.onMessage.addListener(function(message) {
-		if (message.event == 'data') {
-			Kancolle.set(message);
-			return;
+Background.main = function() {
+	var config = {
+		mode: 'pac_script',
+		pacScript: {
+			data: 'function FindProxyForURL(url, host) {\n' +
+			      '  var regexp = /\\/kcsapi\\/api_/;' +
+			      '  if (regexp.test(url)) {\n' +
+			      '    return "PROXY localhost:8549";\n' +
+			      '  }' +
+			      '  return "DIRECT";\n' +
+			      '}'
 		}
-		port2.postMessage(message);
-	});
-});
+	};
 
-chrome.runtime.onConnect.addListener(function(p) {
-	port2 = p;
-});
+	chrome.proxy.settings.set({
+		value: config,
+		scope: 'regular'
+	}, function() {});
+
+	chrome.runtime.onConnectExternal.addListener(function(p) {
+		this.port = p;
+		
+		chrome.windows.create({
+			type: 'popup',
+			url: '../browser/window.html',
+			width: 1100,
+			height: 510
+		});
+		
+		this.port.onMessage.addListener(function(message) {
+			if (message.event == 'data') {
+				var proc = Kancolle.set(message);
+				
+				for (var i = 0; i < proc.length; i++) {
+					this.port2.postMessage(proc[i]);
+				}
+				return;
+			}
+			this.port2.postMessage(message);
+		}.bind(this));
+		
+		Kancolle.start(function(proc) {
+			for (var i = 0; i < proc.length; i++) {
+				this.port2.postMessage(proc[i]);
+			}
+		}.bind(this));
+	}.bind(this));
+}.bind(Background);
+
+onload = Background.main;
