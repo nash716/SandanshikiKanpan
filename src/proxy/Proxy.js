a5755867998675be7aa5165e5277d0282302f185
nash <preflight.cc@gmail.com>
2014-03-18 20:56:39 +0900

    Proxy#stop を実装

--- src/proxy/Proxy.js
+++ src/proxy/Proxy.js
@@ -1,11 +1,14 @@
 var Proxy = function() {
 	this.socketAssign = { };
 	this.serverSocketId = -1;
+	this.isRunning = false;
 };
 
 heir.inherit(Proxy, EventEmitter);
 
 Proxy.prototype.start = function() {
+	if (this.isRunning) return;
+	
 	Util.log('[Proxy] Initializing...');
 	
 	chrome.sockets.tcpServer.create({}, ProxyCallbacks.server.create.bind(this));
@@ -14,8 +17,21 @@ Proxy.prototype.start = function() {
 	
 	chrome.sockets.tcp.onReceiveError.addListener(ProxyCallbacks.tcp.onReceiveError.bind(this));
 	chrome.sockets.tcp.onReceive.addListener(ProxyCallbacks.tcp.onReceive.bind(this));
+	
+	this.isRunning = true;
 };
 
 Proxy.prototype.stop = function() {
-	// TODO
+	if (!this.isRunning) return;
+	
+	chrome.sockets.tcpServer.onAccept.removeListener(ProxyCallbacks.server.onAccept.bind(this));
+	chrome.sockets.tcpServer.disconnect(this.serverSocketId);
+	
+	for (var key in this.socketAssign) {
+		this.socketAssign[key].destroy();
+	}
+	
+	this.serverSocketId = -1;
+	this.socketAssign = { };
+	this.isRunning = false;
 };
